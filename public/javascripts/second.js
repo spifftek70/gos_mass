41006a25c704e556859f3a4db6aabaf8d4d7ad40
dean <spifftek@me.com>
2023-08-30 20:24:54 -0700

    new purchase

--- public/javascripts/second.js
+++ public/javascripts/second.js
@@ -3,11 +3,13 @@ $('.row-clickable').on('click', function() {
 });
 var subTotal = 0;
 var timeTotal = 0;
+var itemNum = 0;
 $(".dropdown-item").on('click', function () {
+    var tester = $('#table-cart > tbody tr').length ;
     var x = $(this).find('.duration').text();
     var y = $(this).find('.price').text();
     $(this).closest('.row').find('.select-values').text(x + " and $" + y);
-    // .card-title
+    var serviceTitel = $(this).closest('.card-body').find('.card-title').text();
     var yy = parseInt(y);
     var xx = parseInt(x);
     // subTotal = yy;
@@ -19,6 +21,7 @@ $(".dropdown-item").on('click', function () {
         subTotal = subTotal + yy;
         newValue = subTotal;
     }
+    itemNum = itemNum +1;
     $('#subtotal').text(" Subtotal of $" + newValue + "   ");
     var yy = parseInt(y);
     // subTotal = yy;
@@ -34,18 +37,34 @@ $(".dropdown-item").on('click', function () {
     var hrs = Math.floor(newTime / 60);
     // Getting the minutes.
     var min = newTime % 60;
-
-    $('#appt-dur').text("  " + hrs + " hrs & " + min + " mins  -  ");
+    $('#table-cart > tbody tr:last').before('<tr>'
+        + '<td>' + itemNum + '</td>' 
+        + '<td>' + serviceTitel + '</td>' 
+        + '<td>' + x + 'mins'
+        + '<td> $' + y + '</td>'
+    + '</tr>');
+    if ($('#table-cart > tbody tr').length >= 2){
+        $('#table-cart > tbody tr:last').remove();
+    }
+    $('#table-cart > tbody:last').append('<tr>'
+    + '<td> Appt Duration: <td>' + hrs + ' hrs & ' + min + ' mins </td>'
+    + '<td> Subtotal Amount: </td>'
+    + '<td> $' + newValue + '</td>'
+    + '</tr>');
+    $('#table-cart > tbody tr:last :eq(0)').css({"color":"white", "background-color":"black"});
+    $('#table-cart > tbody tr:last :eq(1)').css({"color":"white", "background-color":"black"});
+    $('#table-cart > tbody tr:last :eq(2)').css({"color":"white", "background-color":"black"});
+    $('#table-cart > tbody tr:last :eq(3)').css({"color":"white", "background-color":"black"});
+    $('#table-cart').show();
     var closeCheckbox = $(this).closest('.row').find('input[type=checkbox]');
     if (closeCheckbox.prop('checked') == false) {
     closeCheckbox.prop('checked', true);
     } else {
         closeCheckbox.prop('checked', false);
     }
-    $('#bookingForm').append("<div class='form-group col-sm-8 flex-column d-flex'>" +
+    $('#marker').before("<div class='form-group col-sm-8 flex-column d-flex'><p>" +
     "<label class='form-label' for='exampleFormControlInput1'>Service</label>" +
-    "<input class='form-control form-control-lg' id='address' type='text' name='address' placeholder='xxxxx' onblur='validate(5)'/>" +
-    "</div>")
+    "<input class='form-control form-control-lg' id='address' type='text' name='address' onblur='validate(5)' value='" + serviceTitel + "'/></p></div>")
 });
 
 $(".row input:checkbox").on('change', function() {
